f245429a532b8c1d4141525c6e1d12fc20d8b343
Ghassen-bgh <3591@holbertonschool.com>
2021-12-21 16:29:21 +0100

    combo3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 for i in range(0, 100, 1):
-    f = int(i / 10)
-    l = int(i % 10)
-    if f == l:
+    e = int(i / 10)
+    q = int(i % 10)
+    if e == q:
         continue
-    if f > l:
+    if e > q:
         continue
     if i == 89:
-        print("{}{}".format(f, l))
+        print("{}{}".format(e, q))
         break
-    print("{}{}, ".format(f, l), end="")
-    
+    print("{}{}, ".format(e, q), end="")
