8dbe358c182bf13532e630e72c81a0b8b876cb31
Ghassen-bgh <3591@holbertonschool.com>
2021-12-23 16:21:37 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    e = sys.argv
-    size = len(e) - 1
-    ar = "argument" if size is 1 else "arguments"
-    dot = "." if size is 0 else ":"
-    print("{} {}".format(size, ar + dot))
-    for i in range(1, size + 1):
-        print("{}: {}".format(i, e[i]))
+    if len(sys.argv) - 1 == 0:
+        print('{:d} arguments.'.format(len(sys.argv) - 1))
+    elif len(sys.argv) - 1 == 1:
+        print('{:d} argument:'.format(len(sys.argv) - 1))
+    else:
+        print('{:d} arguments:'.format(len(sys.argv) - 1))
+    if len(sys.argv) - 1 > 0:
+        s = 0
+        for x in sys.argv[1:]:
+            s += 1
+            print('{:d}: {:s}'.format(s, x))
