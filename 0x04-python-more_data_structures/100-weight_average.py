5140a1105ab37688c2653a03d3615be19c791414
Ghassen-bgh <3591@holbertonschool.com>
2022-01-07 12:59:56 +0100

    weight av

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,8 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    t, s = 0, 0
+    q, s = 0, 0
     for t in my_list:
-        t += t[0] * t[1]
+        q += t[0] * t[1]
         s += t[1]
-    return t / s
+    return q / s
