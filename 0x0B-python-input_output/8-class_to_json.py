fc768e96ed47da7db3aab2475deffaefb1a5e06f
Ghassen-bgh <3591@holbertonschool.com>
2022-02-01 17:42:53 +0100

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" Class to JSON """
 
 
 def class_to_json(obj):
+    """
+    Args:
+        obj: object to convert to JSON
+    Return:
+        JSON representation of object
+    """
     return vars(obj)
