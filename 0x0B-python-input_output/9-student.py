061f6392f4a7572155dfb349cb16e8550157807b
Ghassen-bgh <3591@holbertonschool.com>
2022-02-01 17:41:28 +0100

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+""" A Student Class """
 
 
 class Student:
+    """ A Student Class """
+
     def __init__(self, first_name, last_name, age):
+        """ Initialize the Student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ Retrieves a dictionary representation of a Student instance """
         return {'first_name': self.first_name,
                 'last_name': self.last_name, 'age': self.age}
