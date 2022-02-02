92529c6f0c5042a3d90c654c286776d8387ebc94
Ghassen-bgh <3591@holbertonschool.com>
2022-02-02 21:56:01 +0100

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,11 @@ Module representation of a list class
 
 
 class MyList(list):
+    """ My list class """
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+        
     def print_sorted(self):
         """
         prints the list in sorted way, ascending order
