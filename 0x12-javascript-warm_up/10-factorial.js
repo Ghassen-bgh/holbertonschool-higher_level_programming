57e50ec95e6eb0068c0499eb9fa080f03fadeba8
Ghassen-bgh <3591@holbertonschool.com>
2022-04-04 23:23:54 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv;
+const num = parseInt(arg[2]);
+
+function factorial (val) {
+  if (!val || val <= 1) {
+    return 1;
+  }
+  return val * factorial(val - 1);
+}
+console.log(factorial(num, num - 1));
